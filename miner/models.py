37027b711e37cdb072c60aab7c5b2d4d7cc98bf5
Daniel Hill <thatdanghill@gmail.com>
2015-05-28 12:30:39 -0700

    Made index not unique

--- miner/models.py
+++ miner/models.py
@@ -8,7 +8,7 @@ class UserProfile(models.Model):
         return self.user.username
 
 class Project(models.Model):
-    user = models.ForeignKey(UserProfile)
+    user = models.ForeignKey(UserProfile, related_name='projects')
     name = models.CharField(max_length=128)
 
     def __unicode__(self):
@@ -16,23 +16,23 @@ class Project(models.Model):
 
 class Page(models.Model):
     name = models.CharField(max_length=128)
-    project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project, related_name='pages')
 
     def __unicode__(self):
         return self.project.__unicode__() + ": " + self.name
 
 class Graph(models.Model):
     name = models.CharField(max_length=128)
-    page = models.ForeignKey(Page)
+    page = models.ForeignKey(Page, related_name='graphs')
 
     def __unicode__(self):
         return self.page.__unicode__() + ": " + self.name
 
 class Point(models.Model):
-    index = models.IntegerField(unique=True)
+    index = models.IntegerField()
     x = models.CharField(max_length = 40)
     y = models.FloatField()
-    graph = models.ForeignKey(Graph)
+    graph = models.ForeignKey(Graph, related_name='points')
 
     def __unicode__(self):
         return self.graph.__unicode__() + ": (" + self.x + "," + str(self.y) + ")"
