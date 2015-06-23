fc74646905ed359ce163bfa8a5f0ed049dfefb7c
Daniel Hill <thatdanghill@gmail.com>
2015-06-23 13:24:23 -0700

    Removed percentages

--- static/js/indextable.js
+++ static/js/indextable.js
@@ -5,9 +5,6 @@ function formatTable() {
 		} else if (!isNaN(parseFloat($(this).html())) && parseFloat($(this).html()) < 0) {
 			$(this).css("color", "red");
 		}
-		if (!isNaN(parseFloat($(this).html()))) {
-			$(this).html($(this).html()+"%");
-		}
 	});
 	
 }
