ce247a683539a916ff0c4bd049c8643ed92fc4fa
Tom Satterthwaite <thomassatterthwaite@Thomass-MacBook-Pro.local>
2015-06-28 09:25:56 +0100

    Graphmanager and logic updated

---
+++ static/js/graphlogic.js
@@ -0,0 +1,10 @@
+/*
+ *
+ *Data in the form:
+ *[[xval, yval],[xval, yval],...]
+ *
+ *function(data) {
+    this.data = someTransformation(data);
+ }
+ 
+ */
--- static/js/graphmanager.js
+++ static/js/graphmanager.js
@@ -96,14 +96,76 @@ RawGraph.prototype = {
 	addButtons : function() {
 		this.container.find('.buttons').prepend($('#tableicon').clone().show());
 		this.container.find('.buttons').prepend($('#calculatoricon').clone().show());
+		
+		var that = this;
+		//!!! FIX
+		this.container.find('#calculatoricon').click(function() {
+			var momstr = "<div id='mom-container' class='graph'></div>";
+			$("#graph-holder").prepend(momstr);
+			var mom = new MonthOnMonthGraph($("#graph-holder").find("#mom-container"), that);
+			mom.calculate(that.data);
+			mom.plot();
+		});
 	}
 }
 
-function CalculatedGraph(container) {
+function CalculatedGraph(container, rawGraph) {
+	this.raw = rawGraph;
 	Graph.call(this, container);
 }
 
 inheritPrototype(CalculatedGraph, Graph);
+
+CalculatedGraph.prototype = {
+	constructor: CalculatedGraph,
+	plot: function() {},
+	calculate: function(data) {}
+}
+
+function MonthOnMonthGraph(container, rawGraph) {
+	this.title = "Month on Month";
+	CalculatedGraph.call(this, container, rawGraph);
+}
+
+inheritPrototype(MonthOnMonthGraph, CalculatedGraph);
+
+MonthOnMonthGraph.prototype = {
+	constructor: MonthOnMonthGraph,
+	plot: function() {
+		this.render();
+		this.addButtons();
+	},
+	
+	calculate: function(data) {
+		for (var i = 1; i < data.length; i++) {
+			this.data[i-1] = [];
+			this.data[i-1][0] = data[i][0];
+			this.data[i-1][1] = (data[i][1]/data[i-1][1] - 1) * 100;
+		}
+	},
+	
+	render: function() {
+		var momGraphLayoutStr = "<div class='title' style='display:inline-block; vertical-align: middle'><p>" + this.title + "</p></div><div class='buttons' style='display:inline-block; margin-left: 25px; vertical-align: middle'></div><div class='view' id='mom-view'></div>";
+		this.container.append(momGraphLayoutStr);
+		this.drawGraph(this.data);
+	},
+	
+	drawGraph: function(data) {
+		var table = new google.visualization.DataTable();
+		table.addColumn('string', "Month");
+		table.addColumn('number', this.title);
+		table.addRows(data);
+		this.options['title'] = this.title;
+		
+		var chart = new google.charts.Line(document.getElementById('mom-view'));
+		chart.draw(table, this.options);
+	},
+	
+	addButtons: function() {
+		
+	}
+	
+}
 
 google.load('visualization', '1.0', {packages:['line', 'table']});
 google.setOnLoadCallback(setUp);
